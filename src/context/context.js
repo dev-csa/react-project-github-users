edbb94b62f412ae6d1b0f7913c4940b2d21f285e
CSA <csarising@gmail.com>
2021-04-28 16:35:05 +0900

    default user setting

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,17 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext(); //이건 컨택스트고 
+
+// Provider, consumer - GithubContext.Provider
+
+const GithubProvider = ({children}) =>{ //이건 컴포넌트임 
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+  
+  return <GithubContext.Provider value={{githubUser, repos, followers}}>{children}</GithubContext.Provider>
+}
+
+export {GithubProvider, GithubContext}
